08cab78603ae28ce7776690aad572859b0827ea9
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-24 00:13:08 +0200

    add cookieParser to server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,14 @@ import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import { apiErrorHandler } from "./middlewares/apiErrorHandler.js";
 import { routeNotFound } from "./middlewares/routeNotFound.js";
+import cookieParser from "cookie-parser";
 
 config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // use it in development mode only and remove it in production
 app.use(morgan("dev"));
