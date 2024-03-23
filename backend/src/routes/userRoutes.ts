73d3c76632fc2afc6995ff4da397500ea2c99310
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-23 23:02:50 +0200

    add login route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,9 +4,11 @@ import { getAllUsers } from "../controllers/users/getAllUsers.js";
 import { userSignup } from "../controllers/users/userSignup.js";
 import { validate } from "../middlewares/validate.js";
 import { userSchema } from "../schemas/userSchema.js";
+import { login } from "../controllers/users/login.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(userSchema), userSignup);
+userRoutes.post("/login", login);
 export default userRoutes;
