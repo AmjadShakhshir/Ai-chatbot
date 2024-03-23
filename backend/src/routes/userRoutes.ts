900bfbacc6139d475f690571e5f64b769e8c58c8
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-23 14:03:09 +0200

    add signup route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { getAllUsers } from "../controllers/users/getAllUsers.js";
+import { userSignup } from "../controllers/users/userSignup.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
-
+userRoutes.post("/signup", userSignup);
 export default userRoutes;
