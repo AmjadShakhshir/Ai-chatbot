b0d76e25f09ad7db5dd8183c71df78c49cc90731
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-23 17:46:46 +0200

    add hashing to passwords

--- backend/src/controllers/users/userSignup.ts
+++ backend/src/controllers/users/userSignup.ts
@@ -1,12 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
+
 import usersService from "../../services/usersService.js";
 import { ApiError } from "../../middlewares/errors/ApiError.js";
 
 export async function userSignup(req: Request, res: Response, next: NextFunction) {
-  const user = await usersService.signUp(req.body);
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    next(ApiError.badRequest("Missing required fields"));
+    return;
+  }
+
+  const hashedPaswword = bcrypt.hashSync(password, 10);
+  const existingUser = await usersService.findByEmail(email);
+
+  if (existingUser) {
+    next(ApiError.badRequest("User already exists"));
+    return;
+  }
+  const user = await usersService.signUp({ name, email, password: hashedPaswword });
   if (!user) {
     next(ApiError.badRequest("User not created"));
     return;
   }
-  res.status(201).json({ user, message: "User created" });
+  res.status(201).json({ id: user._id.toString(), message: "User created" });
 }
