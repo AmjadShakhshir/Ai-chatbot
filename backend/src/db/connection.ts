5f0b4b1cf0dfbe0ae8c57de0cc4b7f6cdc9c7101
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-23 13:08:51 +0200

    create connect file to db

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+const disconnectFromDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB:", error);
+  }
+};
+
+export { connectToDatabase, disconnectFromDatabase };
