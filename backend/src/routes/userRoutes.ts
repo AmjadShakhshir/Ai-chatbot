5b91a26e39c4723cb569f9772b9545bd7b1527db
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-23 17:46:04 +0200

    add validations middleware to user routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import { getAllUsers } from "../controllers/users/getAllUsers.js";
 import { userSignup } from "../controllers/users/userSignup.js";
+import { validate } from "../middlewares/validate.js";
+import { userSchema } from "../schemas/userSchema.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
-userRoutes.post("/signup", userSignup);
+userRoutes.post("/signup", validate(userSchema), userSignup);
 export default userRoutes;
