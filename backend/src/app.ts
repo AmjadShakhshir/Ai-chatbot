d3d2174478f184baa8a83ddc54187d0d011702bf
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-24 15:24:32 +0200

    add cors to server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,10 +5,12 @@ import appRouter from "./routes/index.js";
 import { apiErrorHandler } from "./middlewares/apiErrorHandler.js";
 import { routeNotFound } from "./middlewares/routeNotFound.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 config();
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
