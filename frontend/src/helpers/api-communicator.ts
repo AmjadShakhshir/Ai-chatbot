2f295002412eedf2606b18621e942cbd037d1e4a
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-24 23:41:01 +0200

    add check auth status function

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -12,3 +12,12 @@ export const loginUser = async (email: string, password: string) => {
     throw new Error("Something wrong with Login");
   }
 };
+
+export const checkAuthStatus = async () => {
+  const response = await axios.get("/users/auth-status");
+  if (response.status !== 200) {
+    throw new Error("Unable to authenticate user");
+  }
+  const data = await response.data;
+  return data;
+};
