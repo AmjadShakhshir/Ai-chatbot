0eb8a75908aa5233863dd990ebcf9369a46f18c1
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-25 03:44:45 +0200

    create chat schema

---
+++ backend/src/schemas/chatSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const chatBodySchema = z.object({
+  message: z
+    .string({
+      required_error: "Message is required",
+    })
+    .min(1, { message: "Must be at least 1 characters long" })
+    .max(1000, {
+      message: "Must be at most 1000 characters long",
+    }),
+});
+
+export const chatSchema = z.object({
+  body: chatBodySchema,
+});
