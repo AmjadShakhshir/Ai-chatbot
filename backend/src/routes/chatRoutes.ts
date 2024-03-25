889fcc7950190407029789f20987b974a3f68c23
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-25 03:45:11 +0200

    create new chat route

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
 
+import { verifyToken } from "../utils/token-manager.js";
+import { validate } from "../middlewares/validate.js";
+import { chatSchema } from "../schemas/chatSchema.js";
+import { generateChatCompletion } from "../controllers/chats/generateChatCompletion.js";
+
+// Protected API
 const chatRoutes = Router();
+chatRoutes.post("/new", validate(chatSchema), verifyToken, generateChatCompletion);
 
 export default chatRoutes;
