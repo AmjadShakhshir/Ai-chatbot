ef6d74f0b54f8c25161949f6c95c184c193a77c5
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-26 00:52:45 +0200

    add sendChatsToUser route

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -4,9 +4,11 @@ import { verifyToken } from "../utils/token-manager.js";
 import { validate } from "../middlewares/validate.js";
 import { chatSchema } from "../schemas/chatSchema.js";
 import { generateChatCompletion } from "../controllers/chats/generateChatCompletion.js";
+import { sendChatsToUser } from "../controllers/chats/sendChatsToUser.js";
 
 // Protected API
 const chatRoutes = Router();
 chatRoutes.post("/new", validate(chatSchema), verifyToken, generateChatCompletion);
+chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
 
 export default chatRoutes;
