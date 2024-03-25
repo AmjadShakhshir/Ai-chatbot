b07d2421235f125c988a37107898ca548e2e0b1f
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-26 00:45:29 +0200

    add signup and sendchat request

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -13,6 +13,28 @@ export const loginUser = async (email: string, password: string) => {
   }
 };
 
+export const signupUser = async (email: string, name: string, password: string) => {
+  try {
+    const response = await axios.post(`/users/signup`, { email, name, password });
+    if (response.status !== 200) {
+      throw new Error("Failed to signup");
+    }
+    const data = await response.data;
+    return data;
+  } catch (error: unknown) {
+    throw new Error("Something wrong with Signup");
+  }
+};
+
+export const logoutUser = async () => {
+  const response = await axios.get("/users/logout");
+  if (response.status !== 200) {
+    throw new Error("Failed to logout");
+  }
+  const data = await response.data;
+  return data;
+};
+
 export const checkAuthStatus = async () => {
   const response = await axios.get("/users/auth-status");
   if (response.status !== 200) {
@@ -20,4 +42,17 @@ export const checkAuthStatus = async () => {
   }
   const data = await response.data;
   return data;
+};
+
+export const sendChatRequest = async (message: string) => {
+  try {
+    const response = await axios.post("/chat/new", { message });
+    if (response.status !== 200) {
+      throw new Error("Unable to authenticate user");
+    }
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    throw new Error("Unable to send chat request");
+  }
 };
