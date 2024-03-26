f653caffc64d35c5b59b31030af968f44e4a72c9
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-27 00:42:23 +0200

    add delete route

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -5,10 +5,12 @@ import { validate } from "../middlewares/validate.js";
 import { chatSchema } from "../schemas/chatSchema.js";
 import { generateChatCompletion } from "../controllers/chats/generateChatCompletion.js";
 import { sendChatsToUser } from "../controllers/chats/sendChatsToUser.js";
+import { deleteChats } from "../controllers/chats/deleteChats.js";
 
 // Protected API
 const chatRoutes = Router();
 chatRoutes.post("/new", validate(chatSchema), verifyToken, generateChatCompletion);
 chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
+chatRoutes.delete("/delete", verifyToken, deleteChats);
 
 export default chatRoutes;
