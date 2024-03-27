2e0939beb4c737cc436636567db82ab98e952651
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-27 02:18:15 +0200

    make changes on notgound page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (auth?.user) {
+        return navigate("/chat");
+      }
+      return navigate("/login");
+    }, 3000);
+  }, [auth]);
   return <div>NotFound</div>;
 };
 
