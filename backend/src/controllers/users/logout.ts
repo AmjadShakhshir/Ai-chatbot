fafbaf1ff800763bfc0b283dd4edb4e4f5dabcd2
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-27 02:21:16 +0200

    add logout controller

---
+++ backend/src/controllers/users/logout.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+
+import UserRepo from "../../models/UserModel.js";
+import { ApiError } from "../../middlewares/errors/ApiError.js";
+import { COOKIE_NAME } from "../../utils/constants.js";
+
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = await UserRepo.findById(res.locals.jwtData.id);
+    if (!user) {
+      next(ApiError.forbidden("User Not registered OR Token malfunctioned"));
+    }
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      next(ApiError.forbidden("Permission didn't match"));
+    }
+
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
+    return res.status(200).json({ message: "User Verified", name: user.name, email: user.email });
+  } catch (error) {
+    next(ApiError.internal(error.message));
+  }
+};
